1b7754d6dcab91c1d4f8cd226fd29c861868e86e
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2026-09-15 01:27:33 +0800

    add maplibre map with nuxt ui and strapi backend config

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ClientOnly>
+      <MglMap :map-style="style" :center="center" :zoom="zoom" height="500px">
+        <MglNavigationControl />
+      </MglMap>
+    </ClientOnly>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MglMap, MglNavigationControl } from "@indoorequal/vue-maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+const style =
+  "https://api.maptiler.com/maps/streets/style.json?key=cQX2iET1gmOW38bedbUh";
+const center: [number, number] = [-1.559482, 47.21322];
+const zoom = 8;
+</script>

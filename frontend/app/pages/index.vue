91cc31564703f6957a5ecf895756f65cec153a03
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2026-09-16 21:59:08 +0800

    feat: display sample maplibre map

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <h1> Hello Map </h1>
-    <Map></Map>
-    <h1> Hello Map Leaflet </h1>
-    <MapLeaflet></MapLeaflet>
-  </div>
+    <div>
+        <h1>Hello Map</h1>
+        <ClientOnly>
+            <div class="h-[33vh] w-1/2">
+                <MglMap :map-style="style" :center="center" :zoom="zoom">
+                    <MglNavigationControl />
+                </MglMap>
+            </div>
+        </ClientOnly>
+        <h1>Hello Map Leaflet</h1>
+        <div class="h-[33vh] w-1/2">
+            <MapLeaflet></MapLeaflet>
+        </div>
+    </div>
 </template>
 <script setup lang="ts">
+import type { LngLatLike } from 'maplibre-gl'
 
+const config = useRuntimeConfig()
+const key = config.public.maptilerKey
+const style = `https://api.maptiler.com/maps/streets-v4/style.json?key=${key}`
+const center = [-1.559482, 47.21322] as LngLatLike
+const zoom = 1
 </script>
+
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/leaflet"],
-  css: ["~/assets/css/main.css"],
-});
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    modules: ['@nuxt/ui', '@nuxtjs/leaflet', 'nuxt-maplibre'],
+    css: ['~/assets/css/main.css'],
+    vite: {
+        optimizeDeps: {
+            exclude: ['maplibre-gl'],
+        },
+    },
+    runtimeConfig: {
+        public: {
+            maptilerKey: '',
+            strapiUrl: '',
+        },
+    },
+})
+

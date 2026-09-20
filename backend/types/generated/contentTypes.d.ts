c919537f08f4dce02cb172722d3e4fcdce2a9499
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2026-09-20 10:58:05 +0800

    types: regenerate contentTypes.d.ts after schema updates
    - Auto-generated TypeScript types updated after strapi build

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -616,12 +616,14 @@ export interface ApiFarmerFarmer extends Struct.CollectionTypeSchema {
       'manyToMany',
       'api::barangay.barangay'
     >;
+    contact: Schema.Attribute.BigInteger;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     farmer_code: Schema.Attribute.String &
       Schema.Attribute.Required &
       Schema.Attribute.Unique;
+    farmer_status: Schema.Attribute.Enumeration<['Active', 'Inactive']>;
     farms: Schema.Attribute.Relation<'manyToMany', 'api::farm.farm'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<

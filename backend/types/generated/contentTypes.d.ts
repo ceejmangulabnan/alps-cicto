a8e876ce435e25f584acc5dc505722ef50a0650a
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2026-09-20 14:56:37 +0800

    feat(be/farmer): add farmer status

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -590,6 +590,11 @@ export interface ApiFarmFarm extends Struct.CollectionTypeSchema {
       'oneToMany',
       'api::farm-parcel.farm-parcel'
     >;
+    farmer_status: Schema.Attribute.Enumeration<
+      ['Active', 'Inactive', 'Departed']
+    > &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'Inactive'>;
     farmers: Schema.Attribute.Relation<'manyToMany', 'api::farmer.farmer'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::farm.farm'> &
